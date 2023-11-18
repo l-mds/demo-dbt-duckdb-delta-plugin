2bc03beca4132b13609da208496e566ba70980ab
Aleksandar Milicevic <milicoa97@gmail.com>
2023-11-18 01:07:18 +0100

    add overwrite_partition example

--- dbt_duckdb_delta/models/customer_raw.sql
+++ dbt_duckdb_delta/models/customer_raw.sql
@@ -2,6 +2,6 @@
         materialized='table'    
 )}}
 
-SELECT c_custkey, c_nationkey 
+SELECT *
 FROM {{source("delta_source", "customer")}}
-where c_nationkey = 15
+where c_nationkey = 11
---
+++ dbt_duckdb_delta/models/write/customer_overwrite_partition.sql
@@ -0,0 +1,9 @@
+{{ config(
+    materialized='external_table',
+    plugin = 'delta',
+    location = '/home/aleks/git/open-source/dbt-duckdb-delta-plugin-demo/dbt_duckdb_delta/data/customer',
+    mode = "overwrite_partition",
+    partition_key = ["c_nationkey","c_mktsegment"]
+) }}
+select * from {{ref('customer_raw')}} 
+where c_mktsegment = 'HOUSEHOLD'
